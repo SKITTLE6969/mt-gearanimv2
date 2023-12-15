8307b18433529a055489e38dfa7039156cea523a
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2023-12-15 15:15:01 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,3 +6,7 @@ description 'Gear Changing/Shifting Animation Script Version 2'
 
 shared_script 'config.lua'
 client_script 'client.lua'
+shared_scripts '@ox_lib/init.lua'
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
